b1a5e7bd8173097737a0a19386af09dc57e15f58
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-06-18 14:38:46 +0200

    updated lab assignments

---
+++ multication of 2 numbers/src/mulyiplyoftwonumbers/Main.java
@@ -0,0 +1,12 @@
+package mulyiplyoftwonumbers;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int x = 2;
+	int y = 5;
+	int product = x * y;
+
+	System.out.print(product);
+    }
+}
