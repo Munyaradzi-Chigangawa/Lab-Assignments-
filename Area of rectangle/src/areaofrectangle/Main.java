29ae2c52457392838572ea0fe93856e25bc37fc6
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-06-18 15:19:19 +0200

    updated lab assignments

---
+++ Area of rectangle/src/areaofrectangle/Main.java
@@ -0,0 +1,12 @@
+package areaofrectangle;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int width = 3;
+	int length = 4;
+	int area = width*length;
+
+	System.out.print(area);
+    }
+}
---
+++ counting how many times a letter appear/src/occurrances/Main.java
@@ -0,0 +1,32 @@
+package occurrances;
+
+public class Main {
+    static final int MAX_CHAR = 256;
+
+    static void getOccurringChar (String str) {
+        int count[] = new int[MAX_CHAR];
+        int len = str.length();
+
+        for (int i = 0; i < len; i++)
+            count[str.charAt(i)]++;
+
+        char ch[] = new char[str.length()];
+        for (int i = 0; i <len; i++) {
+            ch[i] = str.charAt(i);
+            int find = 0;
+            for (int j = 0; j < i; j++) {
+                if (str.charAt(i) == ch[j])
+                    find++;
+            }
+            if (find == 1)
+                System.out.print("Number of occurrences of "
+                + str.charAt(i) +" "
+                + "is: " + count[str.charAt(i)]
+                );
+        }
+    }
+    public static void main(String[] args) {
+        String str = "Munya";
+        getOccurringChar(str);
+}
+}
