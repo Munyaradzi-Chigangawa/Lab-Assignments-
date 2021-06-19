110b493a94bf909025d9b77278e915867110fb1f
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-06-19 15:32:24 +0200

    updated lab assignments

---
+++ ASCII/src/ascii/Main.java
@@ -0,0 +1,12 @@
+package ascii;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char ch = 'm';
+	int ascii = ch;
+
+	System.out.print("The ASCII value of " + ch + " "+"is: " + ascii);
+
+    }
+}
