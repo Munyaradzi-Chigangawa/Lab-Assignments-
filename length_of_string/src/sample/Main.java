3124a3c256449c5a701366f0aa5e80e64f4634ac
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-08-18 01:30:26 +0200

    length_of_string

---
+++ length_of_string/src/sample/Main.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Main {
+    public static void main(String[] args) {
+        //declare the String as an object str
+        String str = "Greetings I'm Munya a Junior Java Developer.";
+
+        //length() method of String returns the length of a String S1.
+        int length = str.length();
+        System.out.println("Length of a String is: " + length);
+    }
+}
+
+    //What is String "Length" Method in Java?
+        //This function is used to get the length of string in Java. The string length method returns the number of characters written in the String. This method returns the length of any string which is equal to the number of 16-bit Unicode characters in the string.
