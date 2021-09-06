5036f16fa0482015d3333b7f7b4233513ddc65e6
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-09-06 08:24:35 +0200

    updated lab report

---
+++ Roll Dice/src/Dice.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Dice {
+    public static void main(String[] arguments) {
+        Random generator = new Random();
+        int value = generator.nextInt();
+        System.out.println("The random number is: "
+                + value);
+    }
+}
