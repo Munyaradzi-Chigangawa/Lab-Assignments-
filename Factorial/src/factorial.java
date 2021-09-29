fcc2b1421463352a510f29c0a1396e5e33e69547
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-09-29 18:31:49 +0200

    updated lab report

---
+++ Factorial/src/factorial.java
@@ -0,0 +1,18 @@
+class Factorial {
+
+    public static void main(String[] args) {
+
+        //We will find the factorial of this number
+        int number = 5;
+        long fact = 1;
+        int i = 1;
+        while(i<=number)
+        {
+            fact = fact * i;
+            i++;
+        }
+        System.out.println("Factorial of "+number+" is: "+fact);
+    }
+}
+
+// Factorial of a number n is denoted as n! and the value of n! is: 1 * 2 * 3 * … (n-1) * n
